ed4ee53bee2b7a6b283dde40b54ec95a0c28091f
andreastrekir <astrekir@gmail.com>
2021-04-29 08:59:56 +0300

    First commit

---
+++ exerc2/hola.js
@@ -0,0 +1,45 @@
+var item = prompt ("What you want to add in the basket?");
+function checkBasket(item){ 
+
+var glasses = glasses;
+var books = books;
+var floss = floss;
+
+if (item === glasses){
+	alert(" This item is already in your basket");
+} if (item === books){
+	alert("This item is already in your basket");
+} if (item === floss){
+	alert ("This item is already in your basket");
+} 
+}
+
+
+//alert (checkBasket(prompt( "What you want to add in the basket?")));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
